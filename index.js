04f517382fac2ea1ed35b152daa56978219b0098
DarkCastro24 <104780226+DarkCastro24@users.noreply.github.com>
2024-06-05 20:53:51 -0600

    fix: vercel name.json

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function startServer() {
         await database.connect();
         console.log("Database connected successfully.");
 
-        const port = process.env.PORT || 3500;
+        const port = process.env.PORT || 3000;
         app.listen(port, () => {
             console.log(`Server running on port ${port}`);
         });
